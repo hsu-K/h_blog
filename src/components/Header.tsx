a7e8a21135d62d099822cabdc3945a99f7c70da2
kou-ming <t0937993806@gmail.com>
2025-02-11 21:15:21 +0800

    修改為響應式頁面

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,99 @@
-import { Box, Tabs } from "@radix-ui/themes";
+import { Box, Tabs, Button, DropdownMenu } from "@radix-ui/themes";
 import { Link } from "react-router-dom";
 import { Flex, Text, Container } from "@radix-ui/themes";
+import { useState, useEffect } from "react";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
-const Header = () => { 
+const Header = () => {
+  // 追蹤視窗寬度
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // 桌面版導航
+  const DesktopNav = () => (
+    <Tabs.Root defaultValue="home">
+      <Tabs.List color="cyan">
+        <Box minWidth="70%" ml="7">
+          <Flex align="center" gap="2">
+            <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M9.96424 2.68571C10.0668 2.42931 9.94209 2.13833 9.6857 2.03577C9.4293 1.93322 9.13832 2.05792 9.03576 2.31432L5.03576 12.3143C4.9332 12.5707 5.05791 12.8617 5.3143 12.9642C5.5707 13.0668 5.86168 12.9421 5.96424 12.6857L9.96424 2.68571ZM3.85355 5.14646C4.04882 5.34172 4.04882 5.6583 3.85355 5.85356L2.20711 7.50001L3.85355 9.14646C4.04882 9.34172 4.04882 9.6583 3.85355 9.85356C3.65829 10.0488 3.34171 10.0488 3.14645 9.85356L1.14645 7.85356C0.951184 7.6583 0.951184 7.34172 1.14645 7.14646L3.14645 5.14646C3.34171 4.9512 3.65829 4.9512 3.85355 5.14646ZM11.1464 5.14646C11.3417 4.9512 11.6583 4.9512 11.8536 5.14646L13.8536 7.14646C14.0488 7.34172 14.0488 7.6583 13.8536 7.85356L11.8536 9.85356C11.6583 10.0488 11.3417 10.0488 11.1464 9.85356C10.9512 9.6583 10.9512 9.34172 11.1464 9.14646L12.7929 7.50001L11.1464 5.85356C10.9512 5.6583 10.9512 5.34172 11.1464 5.14646Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" />
+            </svg>
+            <Text size={{initial: "5", sm: "7"}} weight="bold" as="p">hus_K Blog (施工中)</Text>
+          </Flex>
+        </Box>
+        <Link to={"/"}>
+          <Tabs.Trigger value="home">Home</Tabs.Trigger>
+        </Link>
+        <Link to={"/Documents"}>
+          <Tabs.Trigger value="documents">文章列表</Tabs.Trigger>
+        </Link>
+      </Tabs.List>
+    </Tabs.Root>
+  );
+
+  // 手機版導航
+  const MobileNav = () => (
+    <Flex justify="between" align="center" px="4" py="2">
+      <Flex align="center" gap="2">
+        <svg width="24" height="24" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.96424 2.68571C10.0668 2.42931 9.94209 2.13833 9.6857 2.03577C9.4293 1.93322 9.13832 2.05792 9.03576 2.31432L5.03576 12.3143C4.9332 12.5707 5.05791 12.8617 5.3143 12.9642C5.5707 13.0668 5.86168 12.9421 5.96424 12.6857L9.96424 2.68571ZM3.85355 5.14646C4.04882 5.34172 4.04882 5.6583 3.85355 5.85356L2.20711 7.50001L3.85355 9.14646C4.04882 9.34172 4.04882 9.6583 3.85355 9.85356C3.65829 10.0488 3.34171 10.0488 3.14645 9.85356L1.14645 7.85356C0.951184 7.6583 0.951184 7.34172 1.14645 7.14646L3.14645 5.14646C3.34171 4.9512 3.65829 4.9512 3.85355 5.14646ZM11.1464 5.14646C11.3417 4.9512 11.6583 4.9512 11.8536 5.14646L13.8536 7.14646C14.0488 7.34172 14.0488 7.6583 13.8536 7.85356L11.8536 9.85356C11.6583 10.0488 11.3417 10.0488 11.1464 9.85356C10.9512 9.6583 10.9512 9.34172 11.1464 9.14646L12.7929 7.50001L11.1464 5.85356C10.9512 5.6583 10.9512 5.34172 11.1464 5.14646Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" />
+        </svg>
+        <Text size="5" weight="bold">hus_K Blog (施工中)</Text>
+      </Flex>
+      
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Button variant="soft" size="2">
+            <HamburgerMenuIcon width="16" height="16" />
+          </Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content style={{ minWidth: '200px' }}>
+          <DropdownMenu.Item style={{ padding: '12px 16px' }}>
+            <Link 
+              to="/" 
+              style={{ 
+                textDecoration: 'none', 
+                color: 'inherit',
+                fontSize: 'var(--font-size-2)',
+                width: '100%',
+                display: 'block'
+              }}
+            >
+              Home
+            </Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item style={{ padding: '12px 16px' }}>
+            <Link 
+              to="/Documents" 
+              style={{ 
+                textDecoration: 'none', 
+                color: 'inherit',
+                fontSize: 'var(--font-size-2)',
+                width: '100%',
+                display: 'block'
+              }}
+            >
+              文章列表
+            </Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Flex>
+  );
 
   return (
-    <>
-    <Container style={{border: 'solid 0px red'}} maxWidth="100%">
-
-          <Tabs.Root defaultValue="home">
-            <Tabs.List color="cyan">
-
-              <Box minWidth="70%" ml="7">
-
-                <Flex align="center" gap="2">
-                  <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.96424 2.68571C10.0668 2.42931 9.94209 2.13833 9.6857 2.03577C9.4293 1.93322 9.13832 2.05792 9.03576 2.31432L5.03576 12.3143C4.9332 12.5707 5.05791 12.8617 5.3143 12.9642C5.5707 13.0668 5.86168 12.9421 5.96424 12.6857L9.96424 2.68571ZM3.85355 5.14646C4.04882 5.34172 4.04882 5.6583 3.85355 5.85356L2.20711 7.50001L3.85355 9.14646C4.04882 9.34172 4.04882 9.6583 3.85355 9.85356C3.65829 10.0488 3.34171 10.0488 3.14645 9.85356L1.14645 7.85356C0.951184 7.6583 0.951184 7.34172 1.14645 7.14646L3.14645 5.14646C3.34171 4.9512 3.65829 4.9512 3.85355 5.14646ZM11.1464 5.14646C11.3417 4.9512 11.6583 4.9512 11.8536 5.14646L13.8536 7.14646C14.0488 7.34172 14.0488 7.6583 13.8536 7.85356L11.8536 9.85356C11.6583 10.0488 11.3417 10.0488 11.1464 9.85356C10.9512 9.6583 10.9512 9.34172 11.1464 9.14646L12.7929 7.50001L11.1464 5.85356C10.9512 5.6583 10.9512 5.34172 11.1464 5.14646Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                  <Text size="7" weight="bold" as="p">hus_K Bolg (施工中</Text>
-                </Flex>
-              </Box>
-
-              <Link to={"/"}>
-                <Tabs.Trigger value="home">Home</Tabs.Trigger>
-              </Link>
-              <Link to={"/Documents"}>
-                <Tabs.Trigger value="documents">文章列表</Tabs.Trigger>
-              </Link>
-            </Tabs.List>
-          </Tabs.Root>
+    <Container style={{borderBottom: '1px solid var(--gray-a5)'}} maxWidth="100%">
+      {isMobile ? <MobileNav /> : <DesktopNav />}
     </Container>
-    </>
-  )
+  );
 }
 
 export default Header;
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,81 +1,88 @@
 import { Box, Flex, Avatar, Text, Heading, Badge, DataList, AspectRatio, Container } from "@radix-ui/themes";
+
 const HomePage = () => {
   return (
-    <>
-      <Container>
-        <Flex direction={{ initial: 'column', sm: 'row' }}>
-          <Box width="40%" as="div" style={{ borderRight: 'solid 0px white' }}>
-            <Flex direction="column" gap="4" align="center" mt="5" >
-              <Avatar
-                size="9"
-                variant="solid"
-                src="./Avator.png"
-                fallback="A"
-              />
-              <Box >
-                <Flex direction="column" gapY="4" align="center">
-                  <Heading>你好！這裡是hsu_K</Heading>
-                  <Flex gap="2">
-                    <Badge color="orange" size="3">CTF菜雞</Badge>
-                    <Badge color="gold" size="3" variant="solid">魔物"獵"人</Badge>
-                  </Flex>
-                  <DataList.Root size="3" mt="5">
-                    <DataList.Item align="center">
-                      <DataList.Label minWidth="50px" >目前狀態：</DataList.Label>
-                      <DataList.Value>
-                        <Badge color="red" variant="soft" size="2">
-                          專題想不到題目中
-                        </Badge>
-                      </DataList.Value>
-                    </DataList.Item>
-                    <DataList.Item>
-                      <DataList.Label minWidth="50px">Email：</DataList.Label>
-                      <DataList.Value>
-                        <Text>t0937993806@gmail.com</Text>
-                      </DataList.Value>
-                    </DataList.Item>
-                  </DataList.Root>
+    <Container size="4" py="4">
+      <Flex 
+        direction={{ initial: 'column', sm: 'row' }} 
+        gap="6"
+      >
+        {/* 左側個人介紹卡片 */}
+        <Box 
+          width={{ initial: '100%', sm: '30%' }}
+          p="4"
+          style={{ 
+            backgroundColor: 'var(--gray-a2)',
+            borderRadius: 'var(--radius-3)',
+            flexShrink: 0
+          }}
+        >
+          <Flex direction="column" gap="4" align="center">
+            <Avatar
+              size="8"
+              variant="solid"
+              src="./Avator.png"
+              fallback="A"
+              style={{ marginTop: '1rem' }}
+            />
+            
+            <Flex direction="column" gap="3" align="center">
+              <Heading size="6">你好！這裡是hsu_K</Heading>
+              <Flex gap="2" wrap="wrap" justify="center">
+                <Badge color="orange" size="2">CTF菜雞</Badge>
+                <Badge color="gold" size="2" variant="solid">魔物"獵"人</Badge>
+              </Flex>
 
-                  <Flex gap="5" align="center">
-                    <a href="https://github.com/hsu-K" target="_blank">
-                      <img src="./github.svg" width="28px"></img>
-                    </a>
+              <DataList.Root size="2">
+                <DataList.Item align="center">
+                  <DataList.Label minWidth="70px">目前狀態：</DataList.Label>
+                  <DataList.Value>
+                    <Badge color="red" variant="soft" size="2">
+                      專題想不到題目中
+                    </Badge>
+                  </DataList.Value>
+                </DataList.Item>
+                <DataList.Item>
+                  <DataList.Label minWidth="70px">Email：</DataList.Label>
+                  <DataList.Value>
+                    <Text size="2">t0937993806@gmail.com</Text>
+                  </DataList.Value>
+                </DataList.Item>
+              </DataList.Root>
 
-                    <a href="https://discordapp.com/users/612578388179157002" target="_blank">
-                      <img src="./discord.svg" width="25px"></img>
-                    </a>
-                    <a href="https://x.com/697Inventor" target="_blank">
-                      <img src="./twitter.svg" width="25px"></img>
-                    </a>
-                  </Flex>
-                </Flex>
-              </Box>
+              <Flex gap="4" align="center" mt="2">
+                <a href="https://github.com/hsu-K" target="_blank" rel="noopener noreferrer">
+                  <img src="./github.svg" alt="GitHub" width="24" height="24" />
+                </a>
+                <a href="https://discordapp.com/users/612578388179157002" target="_blank" rel="noopener noreferrer">
+                  <img src="./discord.svg" alt="Discord" width="22" height="22" />
+                </a>
+                <a href="https://x.com/697Inventor" target="_blank" rel="noopener noreferrer">
+                  <img src="./twitter.svg" alt="Twitter" width="22" height="22" />
+                </a>
+              </Flex>
             </Flex>
-          </Box>
-          <Box width="15%" style={{ border: 'solid 0px gray' }}></Box>
-          <Box width="100%" height="80vh" style={{ border: 'solid 0px white' }}>
-            <Flex direction="column">
-              <Box width="100%"  height="100%" style={{border: 'solid 0px red'}}>
-                <AspectRatio ratio={16 / 8}>
-                  <img
-                    src="./logbg.jpg"
-                    alt="A house in a forest"
-                    style={{
-                      objectFit: "cover",
-                      width: "100%",
-                      height: "100%",
-                      borderRadius: "var(--radius-2)",
-                    }}
-                  />
-                </AspectRatio>
-              </Box>
-              {/* <GitHubLogoIcon  /> */}
-            </Flex>
-          </Box>
-        </Flex>
-      </Container>
-    </>
-  )
-}
+          </Flex>
+        </Box>
 
-export default HomePage
+        {/* 右側內容區域 */}
+        <Box style={{ flex: "1" }}>
+          <AspectRatio ratio={16 / 9}>
+            <img
+              src="./logbg.jpg"
+              alt="Background"
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100%",
+                borderRadius: "var(--radius-3)",
+              }}
+            />
+          </AspectRatio>
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
+
+export default HomePage;
